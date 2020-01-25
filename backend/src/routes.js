d3910d0808f3936e5c155e1e04545261ce486ef8
Kaio Cesar <tecnico.kaio@gmail.com>
2020-01-25 16:54:38 -0300

    Adicionar user com consulta a api do github

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const { Router } = require('express')
+const axios = require('axios')
+const Dev = require('./models/Dev')
 
 const routes = Router()
 
@@ -11,9 +13,24 @@ routes.get('/devs/:id', (request, response) => {
     return response.json({user: []})
 })
 
-routes.post('/devs', (request, response) => {
-    const { github_username } = request.body
-    return response.json({msg: '', user: []})
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs } = request.body
+
+    const api_response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+    const { name = login, avatar_url, bio } = api_response.data
+
+    const teachs_array = techs.split(',').map(tech => tech.trim())
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: teachs_array
+    })
+
+    return response.json(dev)
 })
 
 routes.put('/devs/:id', (request, response) => {
