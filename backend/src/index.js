25db351eb15a8606e571297e0136dbe0b6f29585
Kaio Cesar <tecnico.kaio@gmail.com>
2020-02-02 14:20:28 -0300

    Porta de execução do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
+const porta = 4003
 
 mongoose.connect((process.env.MONGODSN2020 || 'mongodb+srv://<user>:<pass>@<cluster>/<database>'),{
     useNewUrlParser: true,
@@ -15,6 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(4003, ()=>{
-    console.log(`Voando na porta 3333 🚀`)
+app.listen(porta, ()=>{
+    console.log(`Voando na porta ${porta} 🚀`)
 })
